fff4a01bacda3859942fb60966baea05e0b89300
Tsheden Tshokey <tshedwork@gmail.com>
2025-12-12 15:47:10 +0000

    Button color changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,15 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        'kota-red': '#ff0000',
+        'kota-orange': '#ff7900',
+        'kota-blue': '#80ddd6',
+        'kota-light-green': '#23c9a9',
+        'kota-dark-green': '#108773',
+      },
       fontFamily: {
-        'sans': ['Inter', 'system-ui', '-apple-system', 'sans-serif'],
+        'sans': ['Outfit', 'system-ui', 'sans-serif'],
       },
     },
   },
